3f9469ce7d9e33ee8316df8b8d04324016e91226
maceteligolden <maceteligolden@gmail.com>
2023-12-02 20:53:56 +0100

    feat:add signup hook

---
+++ src/hooks/useSignUp.tsx
@@ -0,0 +1,61 @@
+import { ISignUpInput } from "@/interfaces/auth.interface";
+import { signupOTPValidationSchema, signupValidationSchema } from "@/schemas/auth.schema";
+import { useFormik } from "formik"
+import { useState } from "react";
+
+export default function useSignUp() {
+
+    const [ status, setStatus ] = useState<{type: 'error' | 'success' | null, message: string}>({
+        type: null,
+        message: ''
+    });
+
+    const [showVerification, setShowVerification] = useState<boolean>(false);
+
+    const {handleSubmit, handleChange, errors, values, touched} = useFormik({
+        initialValues: {
+          firstname: '',
+          lastname: "",
+          username: "",
+          phone: "",
+          email: "",
+          password: "",
+        },
+        validationSchema: signupValidationSchema,
+        onSubmit: (values: ISignUpInput) => {
+            console.log(values)
+          setShowVerification(true)
+        },
+    });
+
+    const submitForm = useFormik({
+        initialValues: {
+            otp: ""
+        },
+        validationSchema: signupOTPValidationSchema,
+        onSubmit: (value: {
+            otp: string
+        }) => {
+            console.log({...values, otp: value.otp})
+        }
+    });
+
+    return {
+        status,
+        showVerification,
+        register: {
+            onSubmit: handleSubmit,
+            onChange: handleChange,
+            touched: touched,
+            errors: errors,
+            values: values,
+        },
+        otp : {
+            onSubmit: submitForm.handleSubmit,
+            onChange: submitForm.handleChange,
+            touched: submitForm.touched,
+            errors: submitForm.errors,
+            values: submitForm.values,
+        }
+    }
+}
